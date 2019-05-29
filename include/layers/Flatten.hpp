84344569ce832cdfba3cb4f6e0ef89395b7df84e
Martin4115 <qiaowh@shanghaitech.edu.cn>
2019-05-29 10:27:43 -0700

    update comment

--- include/layers/Flatten.hpp
+++ include/layers/Flatten.hpp
@@ -1,12 +1,24 @@
+///@file Flatten.hpp
+///@brief A layer to process the input data
+///
+/// This kind of layer is used to decrease the dimension
+/// of the input data, transfer the input data to a single 
+/// column vector
+
 #ifndef CS133_LAYER_FLATTEN_HPP
 #define CS133_LAYER_FLATTEN_HPP
 
 #include "../Layer.hpp"
 
+///@brief The class of dense layer
+///
+/// This kind of layer is used to decrease the dimension
+/// of the input data, transfer the input data to a single 
+/// column vector
 class Flatten : public Layer {
 public:
-    Flatten();
-    ~Flatten();
+    Flatten();   ///< Constructor
+    ~Flatten();  ///< Destructor
     void init(int cur_in_size,
               int cur_input_row,
               int cur_input_col,
@@ -16,8 +28,8 @@ public:
               double stride_row,
               double stride_col,
               std::string padding,
-              std::string name);
-    void forward(std::vector<Eigen::MatrixXd> input);
+              std::string name);  ///< Initialize the layer with those arguments
+    void forward(std::vector<Eigen::MatrixXd> input);  ///< Spread forward and get the response
 };
 
 #include "flatten.hpp"
