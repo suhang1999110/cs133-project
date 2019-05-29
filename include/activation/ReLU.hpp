6a2cd7841ce930c7cd85ebab79418e5d7ee270fa
Martin4115 <qiaowh@shanghaitech.edu.cn>
2019-05-29 07:51:15 -0700

    Update comments

--- include/activation/ReLU.hpp
+++ include/activation/ReLU.hpp
@@ -1,17 +1,18 @@
-// TO-DO
-// The ReLU activation function
-// a child class of layer
+///@file ReLU.hpp
+///@brief The ReLU activation function, a child class of layer
 #ifndef CS133_RELU_HPP
 #define CS133_RELU_HPP
 
 #include "../Layer.hpp"
 
-// ReLU function:
-// f(x) = max(x,0)
+
+///@brief Activation function of ReLU
+///
+/// ReLU function: f(x) = max(x,0)
 class Relu : public Layer {
 public:
-    Relu();
-    ~Relu();
+    Relu();   ///<Constructor
+    ~Relu();  ///<Destructor
     void init(int cur_in_size,
               int cur_input_row,
               int cur_input_col,
@@ -21,8 +22,8 @@ public:
               double stride_row,
               double stride_col,
               std::string padding,
-              std::string name);
-    void forward(std::vector<Eigen::MatrixXd> input);
+              std::string name);  ///<Initialize the Layer according to args
+    void forward(std::vector<Eigen::MatrixXd> input);  ///< Spread forward to generate response
 };
 
 #include "reLU.hpp"
--- include/activation/Softmax.hpp
+++ include/activation/Softmax.hpp
@@ -1,15 +1,17 @@
-// TO-DO
-// The Softmax activation function
-// a child class of layer
+///@file ReLU.hpp
+///@brief The Sigmoid activation function, a child class of layer
 #ifndef CS133_SOFTMAX_HPP
 #define CS133_SOFTMAX_HPP
 
 #include "../Layer.hpp"
 
+///@brief Class of Softmax activation function
+///
+/// f(xi) = (e^(xi))/(sigma e^(xj))
 class Softmax : public Layer {
 public:
-	Softmax();
-	~Softmax();
+	Softmax();   ///< Constructor
+	~Softmax();  ///< Destructor
 	void init(int cur_in_size,
 						int cur_input_row,
 						int cur_input_col,
@@ -19,8 +21,8 @@ public:
 						double stride_row,
 						double stride_col,
 						std::string padding,
-						std::string name);
-	void forward(std::vector<Eigen::MatrixXd> input);
+						std::string name);  ///<Initialize the Layer according to args
+	void forward(std::vector<Eigen::MatrixXd> input);  ///< Spread forward to generate response
 };
 
 #include "softmax.hpp"
