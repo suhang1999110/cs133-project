8adefd152ce74663dbdb8a207adcee58c63d51ce
Su Hang <suhang@shanghaitech.edu.cn>
2019-05-29 14:39:26 +0800

    add 10 testcase

--- demo.cpp
+++ demo.cpp
@@ -3,26 +3,26 @@
 #include "include/Net.hpp"
 #include <iostream>
 #include <string>
-#include <opencv2/opencv.hpp>
-#include <opencv2/core/eigen.hpp>
+// #include <opencv2/opencv.hpp>
+// #include <opencv2/core/eigen.hpp>
 
 int main(){
   // file path
-  std::string model_path("test/");
-  std::string weights_path("test/");
-  std::string input_path("test/");
+  std::string model_path("resources/model.json");
+  std::string weights_path("resources/weights.json");
+  std::string input_path("test/test.txt");
   // LeNet-5
   Net net;
   net.init(model_path, weights_path);
-  // 
-  cv::Mat img = cv::imread(input_path);
-  Eigen::MatrixXd input(img.cols, img.rows);
 
-  cv2eigen(img, input);
+  // cv::Mat img = cv::imread(input_path);
+  // Eigen::MatrixXd input(img.cols, img.rows);
 
-  net.forward(input);
+  // cv2eigen(img, input);
 
-  Eigen::MatrixXd output = net.output();
+  
+
+  Eigen::MatrixXd output = net.forward(input);
 
   std::cout<<"Output:\n"<<output;
 
